b9bdf984c0e54d98a63aa7e2eb0adec66100d3f4
Gawon Lee <zczc156@gmail.com>
2023-03-10 21:28:29 +0900

    Prepared implementing DQN in Atari environment

--- rlblock.py
+++ rlblock.py
@@ -1,10 +1,99 @@
 import random
 from collections import namedtuple, deque
 
+import gymnasium as gym
+
 import torch
 import torch.nn as nn
+import torch.cuda as cuda
 
+
+# cuda device
+if cuda.is_available(): device = "cuda"
+else: device = "cpu"
+
+# transition
 Transition = namedtuple("Transition", ("state", "action", "reward", "next_state", "done"))
+
+
+class Environment():
+    def __init__(self, env_name: str, render_mode=None) -> None:
+        self.name = env_name
+        self.render_mode = render_mode
+
+        self._env = gym.make(self.name, render_mode=self.render_mode)  # hidden
+        self.is_vector = len(self._env.observation_space.shape) == 1  # if False -> observation is image
+        self.is_discrete = type(self._env.action_space) == gym.spaces.Discrete  # if False -> action is continuous
+
+        # observation & action dimension
+        if self.is_vector:
+            self.dim_ob = self._env.observation_space.shape[0]
+        else:
+            self.dim_ob = self._env.observation_space.shape
+        
+        if self.is_discrete:
+            self.dim_ac = self._env.action_space.n
+        else:
+            self.dim_ac = self._env.action_space.shape[0]
+
+    def step(self, ac: torch.Tensor):
+        if self.is_discrete: ac = ac.item()
+
+        ob, rwd, terminated, truncated, _ = self._env.step(ac)
+
+        ob_t = torch.tensor(ob, dtype=torch.float32, device=device)
+        rwd_t = torch.tensor([rwd], dtype=torch.float32, device=device)
+        done_t = torch.tensor([terminated or truncated], dtype=torch.float32, device=device)
+
+        return ob_t, rwd_t, done_t  # tensor(ob), tensor([rew]), tensor([done (as 0 or 1)])
+    
+    def reset(self):
+        ob, _ = self._env.reset()
+
+        return torch.tensor(ob, dtype=torch.float32, device=device)
+    
+    def __repr__(self) -> str:
+        ret = "Name: " + self.name
+
+        ret += ", Observation type: "
+        if self.is_vector: ret += "1D Vector"
+        else: ret += "Image"
+
+        ret += ", Action type: "
+        if self.is_discrete: ret += "Discrete"
+        else: ret += "Continuous"
+
+        ret += f", Observation: {self.dim_ob}"
+        ret += f", Action: {self.dim_ac}"
+
+        return ret
+
+class Agent():
+    def __init__(self, ac_space) -> None:
+        self.ac_space = ac_space
+        
+        if self.ac_space == gym.spaces.Discrete:
+            self.is_discrete = True
+        else:
+            self.is_discrete = False
+        
+        self.dim_ac = self.ac_space.n
+
+        self.ob = None
+        self.ac = None
+        self.rwd = None
+        self.next_ob = None
+
+    def get_ac(self):
+        """return random action"""
+        ac = self.ac_space.sample()
+        
+        if self.is_discrete:
+            self.ac = torch.tensor([ac], dtype=torch.int32, device=device)
+        else:
+            self.ac = torch.tensor(ac, dtype=torch.float32, device=device)
+
+        return self.ac  # [ac_1, ac_2, ...]
 
 class ReplayBuffer(object):
     """Replay buffer which saves transition: (s, a, r, s', d)."""
--- snippets_test.py
+++ snippets_test.py
@@ -1,11 +1,62 @@
-import rlutil
 import torch
+import gymnasium as gym
+import numpy as np
 
-x = torch.arange(10)
-y = torch.rand(10)
+from rlblock import Environment
 
-tuple_of_tensors = (x, y)
-print(tuple_of_tensors)
+def ac_test():
+    env = gym.make("MountainCarContinuous-v0", render_mode="human")
 
-ret = rlutil.tuple_of_tensors_to_tensor(tuple_of_tensors)
-print(ret)
+    env.reset()
+
+    # list action
+    env.step([1])
+
+    # np.array action
+    ac_np = np.array([1])
+    env.step(ac_np)
+
+    # torch.Tensor action
+    ac_t = torch.tensor([1])
+    env.step(ac_t)
+
+    """ # float action
+    env.step(1)
+
+    # np.float action
+    ac_np = np.array(1)
+    env.step(ac_np)
+
+    # torch.Tensor with torch.Size([]) action
+    ac_t = torch.tensor(1)
+    env.step(ac_t) """
+
+def env_test():
+    env = gym.make("ALE/Breakout-v5")
+    env.reset()
+
+    print(env.observation_space)
+    print(env.action_space)
+
+    if type(env.action_space) == gym.spaces.Discrete:
+        print("Discrete action space")
+        ob, rwd, terminated, truncated, _ = env.step(0)
+        print(env.action_space.n)
+        print(env.action_space.sample())
+    else:
+        print("Continuous action space")
+        ob, rwd, terminated, truncated, _ = env.step([0])
+        print(env.action_space.shape[0])
+        print(env.action_space.sample())
+
+def tensor_test():
+    t = torch.tensor([True], dtype=torch.float32)
+    print(t)
+
+def main_test():
+    testenv = Environment("ALE/Breakout-v5")
+    print(testenv)
+
+
+if __name__ == "__main__":
+    main_test()
